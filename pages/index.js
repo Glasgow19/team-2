3ae3b4d3bea041224fe2f958a5427683d1983cca
Miles Grant <mlsgrnt@gmail.com>
2019-10-26 16:30:40 +0100

    Fetch article list from Wordpress api

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import Nav from '../components/nav';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+import wp from '../common/wp';
 
-    <Nav />
+const Home = ({ posts }) => {
+  const postList = posts.map(post => {
+    return (
+      <li>
+        <Link
+          href={{ pathname: '/article/[slug]' }}
+          as={`/article/${post.slug}`}
+        >
+          <a>{post.title.rendered}</a>
+        </Link>
+      </li>
+    );
+  });
 
-    <div className="hero">
-      <h1 className="title">Server Rendered React App!</h1>
-      <p className="description">
-        There's so much exciting things happening from here!
-      </p>
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <div>Example content here. CSS is loaded from a static file!</div>
+      <Nav />
+
+      <div className="hero">
+        <h1 className="title">Server Rendered React App!</h1>
+        <p className="description">
+          There's so much exciting things happening from here!
+        </p>
+
+        <div>Example content here. CSS is loaded from a static file!</div>
+        <div>
+          post list:<ul>{postList}</ul>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+Home.getInitialProps = async () => {
+  const posts = await wp.posts();
+  return { posts };
+};
 
 export default Home;
