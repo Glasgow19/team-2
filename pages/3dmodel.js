7397dfd19d9b0976e8148ca39c2d5eb940ce577e
GaganSD <gagandevagiri@gmail.com>
2019-10-26 16:54:26 +0100

    add 3dmodel.js

---
+++ pages/3dmodel.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Head from 'next/head';
+import Nav from '../components/nav';
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Nav />
+
+    <div className="hero">
+      <h1 className="title">Hello, changes!</h1>
+      <p className="description">
+        There's so much exciting things happening from here!
+      </p>
+    </div>
+  </div>
+);
+
+export default Home;
+
