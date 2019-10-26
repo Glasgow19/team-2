18fbbb956aa258bd4c4d5fbf42fa0f6c20f8ece7
2327032p <simona.paulau@gmail.com>
2019-10-26 19:18:49 +0100

    big frontent template

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,5 @@
-import React from 'react';
-import Head from 'next/head';
-import Nav from '../components/nav';
+import ReactDOM from 'react-dom';
+import '../static/index.css';
+import App from '../components/App';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Server Rendered React App!</h1>
-      <p className="description">
-        There's so much exciting things happening from here!
-      </p>
-
-      <div>Example content here. CSS is loaded from a static file!</div>
-    </div>
-  </div>
-);
-
-export default Home;
+ReactDOM.render(<App />, document.getElementById('root'));
