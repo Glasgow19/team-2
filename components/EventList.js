f3769cb43f918da9e6332c142351c24d0a77bdc6
Miles Grant <mlsgrnt@gmail.com>
2019-10-27 08:03:52 +0000

    Add date functions to pretty-display date

--- components/EventList.js
+++ components/EventList.js
@@ -1,9 +1,12 @@
+import { format } from 'date-fns';
 export default ({ events }) => {
   const eventList = events.map(event => {
     return (
       <li key={event.id}>
         <a href={event.url}>{event.name.text}</a>
-        <p className="eventDate">{event.start.local}</p>
+        <p className="eventDate">
+          {format(new Date(event.start.local), 'LLLL do Y, h:mma')}
+        </p>
       </li>
     );
   });
